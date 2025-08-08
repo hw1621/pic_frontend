b86f10c514f5b67395dc687a0684d507fb4f0d42
hw1621 <wanghaoran030401@163.com>
2025-08-08 22:35:09 +0800

    add picture upload component

---
+++ src/components/PictureUpload.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="picture-upload">
+    <a-upload
+      list-type="picture-card"
+      :show-upload-list="false"
+      :custom-request="handleUpload"
+      :before-upload="beforeUpload"
+    >
+      <img v-if="picture?.url" :src="picture?.url" alt="avatar" />
+      <div v-else>
+        <loading-outlined v-if="loading"></loading-outlined>
+        <plus-outlined v-else></plus-outlined>
+        <div class="ant-upload-text">Upload</div>
+      </div>
+    </a-upload>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { PlusOutlined, LoadingOutlined } from '@ant-design/icons-vue'
+import { message } from 'ant-design-vue'
+import type { UploadChangeParam, UploadProps } from 'ant-design-vue'
+import { uploadPictureUsingPost } from '@/api/pictureController.ts'
+
+interface Props {
+  picture?: API.PictureVO
+  onSuccess?: (newPicture: API.PictureVO) => void
+}
+
+const props = defineProps<Props>()
+
+const loading = ref<boolean>(false)
+
+const beforeUpload = (file: UploadProps['fileList'][number]) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG or PNG file!')
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!')
+  }
+  return isJpgOrPng && isLt2M
+}
+
+/**
+ * Upload
+ * @param file
+ */
+const handleUpload = async ({ file }: any) => {
+  loading.value = true
+  try {
+    const params = props.picture ? { id: props.picture.id } : {};
+    const res = await uploadPictureUsingPost(params, {}, file)
+    if (res.data.code === 0 && res.data.data) {
+      message.success('Upload Success')
+      // 将上传成功的图片信息传递给父组件
+      props.onSuccess?.(res.data.data)
+    } else {
+      message.error('Upload fail，' + res.data.message)
+    }
+  } catch (error) {
+    message.error('Upload fail')
+  } finally {
+    loading.value = false
+  }
+}
+
+</script>
+
+<style scoped>
+.picture-upload :deep(.ant-upload) {
+  width: 100% !important;
+  height: 100% !important;
+  min-height: 152px;
+  min-width: 152px;
+}
+
+.picture-upload img {
+  max-width: 100%;
+  max-height: 480px;
+}
+
+.ant-upload-select-picture-card i {
+  font-size: 32px;
+  color: #999;
+}
+
+.ant-upload-select-picture-card .ant-upload-text {
+  margin-top: 8px;
+  color: #666;
+}
+</style>
