a148b2d23de16a461db2feac17109d1841161081
hw1621 <wanghaoran030401@163.com>
2025-08-09 18:05:14 +0800

    complete home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,35 @@
 <template>
   <div id="homePage">
+    <!-- 搜索框 -->
+    <div class="search-bar">
+      <a-input-search
+        placeholder="Search from picture archive.."
+        v-model:value="searchParams.searchText"
+        enter-button="Search"
+        size="large"
+        @search="doSearch"
+      />
+    </div>
+
+    <!-- 分类 + 标签 -->
+    <a-tabs v-model:activeKey="selectedCategory" @change="doSearch">
+      <a-tab-pane key="all" tab="All" />
+      <a-tab-pane v-for="category in categoryList" :key="category" :tab="category" />
+    </a-tabs>
+    <div class="tag-bar">
+      <span style="margin-right: 8px">Tags：</span>
+      <a-space :size="[0, 8]" wrap>
+        <a-checkable-tag
+          v-for="(tag, index) in tagList"
+          :key="tag"
+          v-model:checked="selectedTagList[index]"
+          @change="doSearch"
+        >
+          {{ tag }}
+        </a-checkable-tag>
+      </a-space>
+    </div>
+
     <!-- 图片列表 -->
     <a-list
       :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }"
@@ -10,7 +40,7 @@
       <template #renderItem="{ item: picture }">
         <a-list-item style="padding: 0">
           <!-- 单张图片 -->
-          <a-card hoverable>
+          <a-card hoverable @click="doClickPicture(picture)">
             <template #cover>
               <img
                 style="height: 180px; object-fit: cover"
@@ -22,7 +52,7 @@
               <template #description>
                 <a-flex>
                   <a-tag color="green">
-                    {{ picture.category ?? '默认' }}
+                    {{ picture.category ?? 'Default' }}
                   </a-tag>
                   <a-tag v-for="tag in picture.tags" :key="tag">
                     {{ tag }}
@@ -37,11 +67,16 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { computed, onMounted, reactive, ref } from 'vue'
-import { listPictureVoByPageUsingPost } from '@/api/pictureController.ts'
+import {
+  listPictureTagCategoryUsingGet,
+  listPictureVoByPageUsingPost,
+} from '@/api/pictureController.ts'
 import { message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const dataList = ref([])
 const total = ref(0)
 const loading = ref(true)
@@ -69,10 +104,31 @@ const pagination = computed(() => {
   }
 })
 
+const doSearch = () => {
+  // 重置搜索条件
+  searchParams.current = 1
+  fetchData()
+}
+
 // 获取数据
 const fetchData = async () => {
   loading.value = true
-  const res = await listPictureVoByPageUsingPost(searchParams)
+  const params = {
+    ...searchParams,
+    tags: [] as string[],
+  }
+  if (selectedCategory.value !== 'all') {
+    console.log("Selected category:", selectedCategory.value)
+    params.category = selectedCategory.value
+  }
+
+  selectedTagList.value.forEach((useTag, index) => {
+    if (useTag) {
+      params.tags.push(tagList.value[index])
+    }
+  })
+
+  const res = await listPictureVoByPageUsingPost(params)
   if (res.data.data) {
     dataList.value = res.data.data.records ?? []
     total.value = res.data.data.total ?? 0
@@ -82,8 +138,49 @@ const fetchData = async () => {
   loading.value = false
 }
 
-// 页面加载时请求一次
+const categoryList = ref<string[]>([])
+const selectedCategory = ref<string>('all')
+const tagList = ref<string[]>([])
+const selectedTagList = ref<string[]>([])
+
+// 获取标签和分类选项
+const getTagCategoryOptions = async () => {
+  const res = await listPictureTagCategoryUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    // 转换成下拉选项组件接受的格式
+    categoryList.value = res.data.data.categoryList ?? []
+    tagList.value = res.data.data.tagList ?? []
+  } else {
+    message.error('Get category tags fail，' + res.data.message)
+  }
+}
+
+const doClickPicture = (picture: API.PictureVO) => {
+  router.push({
+    path:`/picture/${picture.id}`
+  })
+}
+
+onMounted(() => {
+  getTagCategoryOptions()
+})
+
 onMounted(() => {
   fetchData()
 })
 </script>
+
+<style>
+#homePage {
+  margin-bottom: 16px;
+}
+
+#homePage .search-bar {
+  max-width: 480px;
+  margin: 0 auto 16px;
+}
+
+#homePage .tag-bar {
+  margin-bottom: 16px;
+}
+</style>
